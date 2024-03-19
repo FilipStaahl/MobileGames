96a47500413316437b32a681c97bc859ac7e5d11
FilipStaahl <filip.stahl@student.forsbergsskola.se>
2024-03-19 12:04:18 +0100

    Update

--- Assets/Scripts/InventoryScript.cs
+++ Assets/Scripts/InventoryScript.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventoryScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Image itemPrefab; // Reference to the item image prefab
+    public Color[] itemColors; // Array of colors for the items
+
+    private RectTransform contentRect; // Reference to the Scroll Rect's content area
+
     void Start()
     {
-        
-    }
+        if (itemPrefab == null || itemColors.Length == 0)
+        {
+            Debug.LogError("Please assign item prefab and color array!");
+            return;
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        contentRect = GetComponentInChildren<ScrollRect>().content; // Get the Scroll Rect's content
+
+        // Loop through colors and instantiate items
+        for (int i = 0; i < itemColors.Length; i++)
+        {
+            Image newItem = Instantiate(itemPrefab, contentRect); // Instantiate inside Scroll Rect's content
+            newItem.color = itemColors[i]; // Set the color of the new item
+        }
     }
 }
